d8ab8c2f917d8ce6ca59039c9d977a58553c315b
Haroro <hutao-haroro@qq.com>
2020-04-03 01:42:52 +0800

    增加命令

---
+++ Haroro.iphoneXs.sh
@@ -0,0 +1,1 @@
+flutter clean && rm -rf ios/Flutter/App.framework && flutter run -d 00008020-000865140E28002E
--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+      MyApp(
+        items: List<String>.generate(1000, (i) => 'Item $i'),
+      ),
+    );
 
 String appName = 'TimeCaps';
-String headIcon = 'https://tuchong.pstatp.com/314506/f/4196061.jpg';
-String phone = '18119921782';
-String username = 'SunnyHaroro';
-String card = '6288888888888888';
-String money = '3000.00';
 
 class MyApp extends StatelessWidget {
-  //拨打电话
-  _call() async {
-    const url = 'tel:18119921782';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
-  //发送短信
-  _message() async {
-    const url = 'sms:18119921782';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
-  //打开外部浏览器
-  _openBrower() async {
-    const url = 'https://flutter.dev';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
-  //打开外部应运用
-  _openOtherApp() async {
-    /**
-     * weixin://
-     * alipays://
-     */
-    const url = 'alipays://';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
+  final List<String> items;
+  MyApp({Key key, @required this.items}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -63,46 +21,89 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text(appName),
         ),
-        body: ListView(
-          children: <Widget>[
-            Image.asset(
-              'assets/images/person_back.jpg',
-              height: 100.0,
-              fit: BoxFit.cover,
-            ),
-            ListTile(
-              leading: ClipOval(
-                child: Image.asset(
-                  'assets/images/avatar.jpeg',
-                  height: 30.0,
-                  fit: BoxFit.cover,
-                  alignment: Alignment.center,
-                ),
-              ),
-              title: Text(
-                username,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ),
-              subtitle: Text(phone),
-              trailing: GestureDetector(
-                child: Icon(Icons.phone),
-                onTap: () => _call(),
-              ),
-              enabled: true,
-              selected: false,
-            ),
-            ListTile(
-              leading: Icon(Icons.language),
-              title: Text(card),
-              trailing: GestureDetector(
-                child: Icon(Icons.open_in_new),
-                onTap: () => _openBrower(),
-              ),
-            )
-          ],
-        ),
+        body: MyList()
       ),
     );
+  }
+}
+
+class MyList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        Image.asset(
+          'assets/images/person_back.jpg',
+          height: 100.0,
+          fit: BoxFit.cover,
+        ),
+        ListTile(
+          leading: ClipOval(
+            child: Image.asset('assets/images/avatar.jpeg',
+                height: 30.0, fit: BoxFit.cover),
+          ),
+          title: Text('Haroro', maxLines: 2, overflow: TextOverflow.ellipsis),
+          subtitle: Text('18119921782'),
+          trailing: GestureDetector(
+            child: Icon(Icons.sms),
+            onTap: () => _message(),
+          ),
+          enabled: true,
+          selected: false,
+        ),
+        ListTile(
+          leading: Icon(Icons.language),
+          title: Text('www.baidu.com'),
+          trailing: GestureDetector(
+            child: Icon(Icons.open_in_new),
+            onTap: () => _openBrower(),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+//拨打电话
+_call() async {
+  const url = 'tel:18119921782';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+//发送短信
+_message() async {
+  const url = 'sms:18119921782';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+//打开外部浏览器
+_openBrower() async {
+  const url = 'https://baidu.com';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+//打开外部应运用
+_openOtherApp() async {
+  /**
+     * weixin://
+     * alipays://
+     */
+  const url = 'alipays://';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
